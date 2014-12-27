e38d0eb049d13fb92c62660e60f869e91034dc9f
ayaz-ahmed <ayazahmed_@hotmail.com>
2014-12-27 15:42:53 +0500

    Test-1 (27-dec-2014)
    Remove the duplication and make general 'times' method.

--- Dollar.java
+++ Dollar.java
@@ -1,13 +1,13 @@
 
 public class Dollar {
 
-	int amount = 10;
+	int amount;
 	
 	public Dollar(int amount){
-		
+		this.amount = amount;
 	}
 	
 	public void times(int multiplier){
-		
+		amount *= multiplier;
 	}
 }
