bf70c776a765fb11eab17e6c47278f73249ebba7
ayaz-ahmed <ayazahmed_@hotmail.com>
2014-12-27 16:13:12 +0500

    Test - 2 (27-12-2014)
    change in 'MultiCurrencyTest' make the multiplier dynamic to get correct dollar w.r.t ratio or multiplier.

--- MultiCurrencyTest.java
+++ MultiCurrencyTest.java
@@ -1,11 +1,18 @@
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MultiCurrencyTest{
+
+public class MultiCurrencyTest {
+
 	@Test
 	public void testMultiplication(){
 		Dollar fiveDollar  = new Dollar(5);
-		fiveDollar.times(2);
-		Assert.assertEquals(10, fiveDollar.amount);
+		Dollar product = fiveDollar.times(2);
+		assertEquals(10, product.amount);
+		
+		product = fiveDollar.times(3);
+		assertEquals(15, product.amount);
 	}
 }
