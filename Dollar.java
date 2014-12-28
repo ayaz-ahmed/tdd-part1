c24045f6c6c8baf04cf281669813e8942645233e
ayaz-ahmed <ayazahmed_@hotmail.com>
2014-12-28 19:30:34 +0500

    Test - 6 (28 - 12 - 2014)
    Eliminate equals method by extending from money.

--- Dollar.java
+++ Dollar.java
@@ -1,18 +1,11 @@
 
-public class Dollar {
+public class Dollar extends Money{
 
-	private int amount;
-	
 	public Dollar(int amount){
 		this.amount = amount;
 	}
 	
 	public Dollar times(int multiplier){
 		return new Dollar(amount * multiplier);
-	}
-	
-	public boolean equals(Object object){
-		Dollar dollar = (Dollar)object;
-		return this.amount == dollar.amount;
 	}
 }
