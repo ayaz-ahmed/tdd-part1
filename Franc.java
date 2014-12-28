f5af57677cdc241ef80920b8677d15daeb1a3d8d
ayaz-ahmed <ayazahmed_@hotmail.com>
2014-12-28 19:31:57 +0500

    Test - 6 (28 - 12 - 2014)
    Eliminate equals method moved it to money superclass.

--- Franc.java
+++ Franc.java
@@ -1,18 +1,11 @@
 
-public class Franc {
+public class Franc extends Money{
 
-	private int amount;
-	
 	public Franc(int amount){
 		this.amount = amount;
 	}
 	
 	public Franc times(int multiplier){
 		return new Franc(amount*multiplier);
-	}
-	
-	public boolean equals(Object object){
-		Franc franc = (Franc)object;
-		return amount == franc.amount;
 	}
 }
