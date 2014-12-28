b6df286b237c513a25dc7fe96ba318d7cf3353fe
ayaz-ahmed <ayazahmed_@hotmail.com>
2014-12-28 15:22:34 +0500

    Test - 4 (28 - 12 - 2014)
    make the dollar instance variable private and use equal method to compare in assertion method.

--- MultiCurrencyTest.java
+++ MultiCurrencyTest.java
@@ -9,11 +9,9 @@ public class MultiCurrencyTest {
 	@Test
 	public void testMultiplication(){
 		Dollar fiveDollar  = new Dollar(5);
-		Dollar product = fiveDollar.times(2);
-		assertEquals(10, product.amount);
 		
-		product = fiveDollar.times(3);
-		assertEquals(15, product.amount);
+		assertEquals(new Dollar(10), fiveDollar.times(2));
+		assertEquals(new Dollar(15), fiveDollar.times(3));
 	}
 	
 	@Test 
