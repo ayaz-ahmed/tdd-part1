4795e263c141fc81b9a550312e4f8da42e9ef174
ayaz-ahmed <ayazahmed_@hotmail.com>
2015-01-03 12:32:40 +0500

    Test - 13 (02 - Jan - 2015)
    Using a polymorphism to eliminate explicit type checking

--- Bank.java
+++ Bank.java
@@ -1,7 +1,7 @@
 
 public class Bank {
 
-	public Money reduce(Expression sourse, String to){
-		return Money.dollar(10);
+	public Money reduce(Expression source, String to){
+		return source.reduce(to);
 	}
 }
